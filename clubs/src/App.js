4c64bc26e30738f09a8eb985f2272196e7f2d9b8
Nicolas <ruiznico360@gmail.com>
2021-02-15 21:44:02 -0500

    l

---
+++ clubs/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Test page.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
