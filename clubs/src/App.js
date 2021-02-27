3c3c52d74bff386f4e11db76c9215811f7849183
JJ Kanu <jjkanu20@gmail.com>
2021-02-26 20:53:22 -0500

    Added import to second incomplete screeen

--- clubs/src/App.js
+++ clubs/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import ClubPage from './screens/ClubPage.js';
+import ProfilePage from './screens/ProfilePage';
 
 class App extends React.Component {
 	render() {
 	    	return (
 	       	<ClubPage/>
+          //  <ProfilePage/>
 	    	);  
     	}
 }
