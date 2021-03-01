d64b1bad690c7653007c99b788bc865220765606
JJ Kanu <jjkanu20@gmail.com>
2021-03-01 15:18:55 -0500

    Added router and switch to app

--- clubs/src/App.js
+++ clubs/src/App.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import './App.css';
 import ClubPage from './screens/ClubPage.js';
+import SignInPage from './screens/SignInPage.js'
 import ProfilePage from './screens/ProfilePage';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 class App extends React.Component {
 	render() {
 	    	return (
-	       	<ClubPage/>
-          //  <ProfilePage/>
+				<div>
+					<Router>
+						<Switch>
+							{/* <ClubPage/>
+							<ProfilePage/> */}
+							<Route exact path="/" component={ClubPage} />
+							<Route path="/login" component={SignInPage}/>
+						</Switch>
+					</Router>
+				</div>
 	    	);  
-    	}
+    }
 }
 
 export default App;
