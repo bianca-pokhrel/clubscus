e735c5a10419fa13e1e21a3b663c35838aec6add
ruiznico360 <ruiznico360@gmail.com>
2021-03-02 18:30:06 -0500

    Comment section work

--- clubs/src/components/ClubPost/CommentSection.js
+++ clubs/src/components/ClubPost/CommentSection.js
@@ -1,33 +1,26 @@
 import React from "react"
 import './CommentSection.css'
 import 'antd/dist/antd.css';
-import { Input } from 'antd'
-import { AudioOutlined } from '@ant-design/icons';
+import { Input, Button } from 'antd'
 
-
+//Input id="comment_bar" placeholder="Enter comment here" size="large"
 class CommentSection extends React.Component{
 	render() {
 		const post = this.props.post
 		const comments = this.props.comments
 		
-		const f = () => {
-			return ( <AudioOutlined
-    style={{
-      fontSize: 16,
-      color: '#1890ff',
-    }}
-  />)
-		}
-		
 		return(
-		    <div id="comment_container">
-		    	<Input
-				placeholder="input search text"
-				enterButton="Search"
-			 	size="large"
-			 	suffix={f()}
-			/>
-		    </div>
+			<div id="comment_container">
+				<div id="comment_msg_container">
+				    	<div id="comment_bar">
+						<Input placeholder="Enter comment here" size="large"/>
+				    	</div>
+					<Button id="comment_input">
+						<p>Post</p>
+					</Button>
+				</div>
+				&nbsp;
+		    	</div>
         	)
     	}
 
