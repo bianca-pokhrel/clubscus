76974ef8d9f03440655cfeb0a83605739785742b
JJ Kanu <jjkanu20@gmail.com>
2021-03-03 20:59:45 -0500

    Changed curgroups to use antd list

--- clubs/src/components/ProfilePage/CurGroups.js
+++ clubs/src/components/ProfilePage/CurGroups.js
@@ -1,35 +1,41 @@
 import React from "react";
 import './CurGroups.css'
 
-// let myGroups = {
-//     "CSC309 Project Group": "This is a group for the project",
-//     "UofT Cooking Club": "'Official' club for cooking on campus"
-// }
+import { List, Typography } from 'antd';
 
+const groupData = [
+    {groupName: "CSC309 Project Group", description: "This is a group for the project", url: "/", club: false},
+    {groupName: "UofT Puppies Club", description: "Puppy stuff", url: "/", club: true},
+];
 
 class CurGroups extends React.Component{
 
-    // constructor(props){
-    //     myGroups = {}
-    // }
-
-    // test = (e) => {
-    //     console.log(this.myGroups)
-    // }
-
     render (){
         return(
-           <div id="curGroupsContainer">
-               <div id="headerContainer">
-                    <h3>Joined Groups</h3>
-               </div>
-               <div id="groupListContainer">
-                    <ul>
-                        <li className="listedGroup">CSC309 Project Group</li>
-                        <li className="listedClub">Uoft Cooking Club</li>
-                    </ul>
-               </div>
-           </div>
+        //    <div id="curGroupsContainer">
+        //        <div id="headerContainer">
+        //             <h3>Joined Groups</h3>
+        //        </div>
+        //        <div id="groupListContainer">
+        //             <ul>
+        //                 <li className="listedGroup">CSC309 Project Group</li>
+        //                 <li className="listedClub">Uoft Cooking Club</li>
+        //             </ul>
+        //        </div>
+        //    </div>
+        <List
+            bordered
+            dataSource={groupData}
+            renderItem={group => (
+            <List.Item>
+                <List.Item.Meta
+                // avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
+                title={<a href={group.url}>{group.groupName}</a>}
+                description={group.description}
+                />
+            </List.Item>
+            )}
+        />
         )
     }
 }
