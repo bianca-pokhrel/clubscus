254780f3af0794e2f7e825fa8194f2c226779251
ruiznico360 <ruiznico360@gmail.com>
2021-04-09 23:42:58 -0400

    mongoose fix

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,12 +1,12 @@
 /* This module will hold our connection to 
    our mongo server through the Mongoose API.
    We will access the connection in our express server. */
-const mongoose = require('mongoose')
+   const mongoose = require('mongoose')
 
-/* Connnect to our database */
-// Get the URI of the local database, or the one specified on deployment.
-const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/StudentAPI'
-
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true});
-
-module.exports = { mongoose }  // Export the active connection.
+   /* Connnect to our database */
+   // Get the URI of the local database, or the one specified on deployment.
+   const mongoURI = process.env.MONGODB_URI || 'mongodb+srv://biancap:mymongo309@cluster0.0jr7p.mongodb.net/test'
+   
+   mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true});
+   
+   module.exports = { mongoose }  // Export the active connection.
